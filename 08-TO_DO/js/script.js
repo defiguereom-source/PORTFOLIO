c5040482b33953ebe09137134582cebf0a9416e5
daniel182730 <daniel182730@gmail.com>
2026-03-27 10:26:52 -0400

    feat(TO_DO): agregando diseño y agregando funcionalidad en sus respectivos botones

--- 08-TO_DO/js/script.js
+++ 08-TO_DO/js/script.js
@@ -12,6 +12,17 @@ formulario.addEventListener('submit', e => {
     setTarea(e); 
 });
 
+document.addEventListener('DOMContentLoaded',() =>{
+    if(localStorage.getItem('tareas')){
+        tareas = JSON.parse(localStorage.getItem('tareas')) 
+    }
+    pintarTareas();
+})
+
+lista_tareas.addEventListener('click', e => {
+    btnAccion(e);
+});
+
 //valida si el input esta vacio en el dado caso de que no crea una tarea 
 const setTarea = e => {
     if(input.value.trim() === ''){
@@ -26,25 +37,61 @@ const setTarea = e => {
     }
 
     tareas[tarea.id] = tarea
-    console.log(tareas); 
     
     formulario.reset(); 
-    input.focus();
+    input.focus();  
 
     pintarTareas();
 };
 
 // crea un clone y lo muestra en el html utilizando template(es como una plantilla)
 const pintarTareas = () => {
-    lista_tareas.innerHTML = ''
 
+    localStorage.setItem('tareas', JSON.stringify(tareas));
+
+    if ( Object.values(tareas).length === 0){
+        lista_tareas.innerHTML = `
+            <div class="alert-darck">
+                No hay tareas pendientes 😍
+            </div>
+        `
+        return 
+    }
+    lista_tareas.innerHTML = ''
     Object.values(tareas).forEach(tarea => {
         const clone = template_alert.cloneNode(true);
-
         clone.querySelector('p').textContent = tarea.text;
-        
+
+        if(tarea.estado){
+            clone.querySelector('.container_tarea').classList.add('completada');
+            clone.querySelectorAll('.fas')[0].classList.replace('fa-circle-check','fa-rotate-left');
+            clone.querySelector('p').style.textDecoration = 'line-through' 
+        }
+
+
+        clone.querySelectorAll('.fas')[0].dataset.id = tarea.id
+        clone.querySelectorAll('.fas')[1].dataset.id = tarea.id
         fragment.appendChild(clone);
     }) 
 
     lista_tareas.appendChild(fragment);
 }
+
+const btnAccion = e => {
+    if (e.target.classList.contains('fa-circle-check')){
+        tareas[e.target.dataset.id].estado = true
+        pintarTareas()
+    }
+
+    if(e.target.classList.contains('fa-circle-minus')){
+        delete tareas[e.target.dataset.id]
+        pintarTareas()
+    }
+
+    if (e.target.classList.contains('fa-rotate-left')){
+        tareas[e.target.dataset.id].estado = false
+        pintarTareas()
+    }
+
+    e.stopPropagation()
+};
